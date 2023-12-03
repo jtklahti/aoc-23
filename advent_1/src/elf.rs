4684792732a988f6ecdb15a1a9e92793e23d92ae
Jukka Lahtinen <jukka.lahtinen@kipina.fi>
2023-12-04 00:43:08 +0200

    2023 1.2 passed

--- advent_1/src/elf.rs
+++ advent_1/src/elf.rs
@@ -1,23 +1,67 @@
 mod tests;
 
+fn parse_digit_textual(s: &str) -> u32 {
+    let digit_map = [
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ];
+    for (str, num) in digit_map {
+        if s.len() >= str.len() && str == &s[0..str.len()] {
+            return num;
+        }
+    }
+    return 0;
+}
+
+fn parse_digit_numeral(s: &str) -> u32 {
+    s.chars()
+        .nth(0)
+        .unwrap_or_default()
+        .to_digit(10)
+        .unwrap_or_default()
+}
+
+pub fn parse_digit(s: &str) -> u32 {
+    let numeral = parse_digit_numeral(s);
+    if numeral > 0 {
+        return numeral;
+    } else {
+        parse_digit_textual(s)
+    }
+}
+
 pub fn parse_line(line: String) -> u32 {
     let mut first: u32 = 0;
     let mut last: u32 = 0;
-    for c in line.chars() {
-        if !c.is_ascii_digit() {
+    if 0 == line.len() {
+        return 0
+    }
+    for index in 0..line.len() {
+        let str = &line[index..];
+        let digit = parse_digit(str);
+        if 0 == digit {
             continue;
         }
         if first == 0 {
-            first = c.to_digit(10).unwrap_or_default();
+            first = digit;
         }
-        last = c.to_digit(10).unwrap_or_default();
+        last = digit;
     }
     return 10 * first + last;
 }
 
 pub fn parse_content(content: String) -> i32 {
-    let numbers: Vec<i32> = content.lines().into_iter().map(
-        |line| parse_line(line.to_string()) as i32
-    ).collect();
+    let numbers: Vec<i32> = content
+        .lines()
+        .into_iter()
+        .map(|line| parse_line(line.to_string()) as i32)
+        .collect();
     numbers.iter().sum()
 }
--- advent_1/src/elf/tests.rs
+++ advent_1/src/elf/tests.rs
@@ -1,4 +1,45 @@
 #[cfg(test)]
+mod parse_digit {
+    use crate::elf::parse_digit;
+
+    #[test]
+    fn parse_digit_zero() {
+        assert_eq!(parse_digit(&"a".to_string()), 0);
+        assert_eq!(parse_digit(&"asdffsd".to_string()), 0);
+        assert_eq!(parse_digit(&"".to_string()), 0);
+    }
+
+    #[test]
+    fn parse_digit_str_simple() {
+        assert_eq!(parse_digit(&"one".to_string()), 1);
+        assert_eq!(parse_digit(&"two".to_string()), 2);
+        assert_eq!(parse_digit(&"three".to_string()), 3);
+        assert_eq!(parse_digit(&"four".to_string()), 4);
+        assert_eq!(parse_digit(&"five".to_string()), 5);
+        assert_eq!(parse_digit(&"six".to_string()), 6);
+        assert_eq!(parse_digit(&"seven".to_string()), 7);
+        assert_eq!(parse_digit(&"eight".to_string()), 8);
+        assert_eq!(parse_digit(&"nine".to_string()), 9);
+    }
+
+    #[test]
+    fn parse_digit_str_between() {
+        assert_eq!(parse_digit(&"one332".to_string()), 1);
+        assert_eq!(parse_digit(&"twosdfd".to_string()), 2);
+        assert_eq!(parse_digit(&"threeXXc2".to_string()), 3);
+    }
+
+    #[test]
+    fn parse_digit_num() {
+        assert_eq!(parse_digit(&"2".to_string()), 2);
+        assert_eq!(parse_digit(&"3".to_string()), 3);
+        assert_eq!(parse_digit(&"2aazza".to_string()), 2);
+        assert_eq!(parse_digit(&"3twox".to_string()), 3);
+        assert_eq!(parse_digit(&"9xyzone".to_string()), 9);
+    }
+}
+
+#[cfg(test)]
 mod parse_line {
     use crate::elf::parse_line;
 
@@ -9,28 +50,46 @@ mod parse_line {
 
     #[test]
     fn parse_line_simple() {
-        assert_eq!(parse_line("20".to_string()), 20);
+        assert_eq!(parse_line("21".to_string()), 21);
         assert_eq!(parse_line("2".to_string()), 22);
         assert_eq!(parse_line("52".to_string()), 52);
     }
 
     #[test]
-    fn parse_line_complex() {
+    fn parse_line_with_chars() {
         assert_eq!(parse_line("aa2xxx".to_string()), 22);
         assert_eq!(parse_line("aa8d2d1sd4xyx".to_string()), 84);
     }
 
     #[test]
-    fn parse_line_examples() {
+    fn parse_line_examples1() {
         let inputs = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"];
         let results = [12, 38, 15, 77];
         for i in 0..results.len() {
-            assert_eq!(parse_line(inputs[i].to_string()), results[i]); //results[i])
+            assert_eq!(parse_line(inputs[i].to_string()), results[i]);
+        }
+    }
+
+    #[test]
+    fn parse_line_examples2() {
+        let inputs = [
+            "two1nine",
+            "eightwothree",
+            "abcone2threexyz",
+            "xtwone3four",
+            "4nineeightseven2",
+            "zoneight234",
+            "7pqrstsixteen",
+        ];
+        let results = [29, 83, 13, 24, 42, 14, 76];
+        for i in 0..results.len() {
+            assert_eq!(parse_line(inputs[i].to_string()), results[i]);
         }
     }
 }
 
-mod parse_lines {
+#[cfg(test)]
+mod parse_content {
     use crate::elf::parse_content;
 
     #[test]
@@ -44,17 +103,32 @@ mod parse_lines {
         1
         1
         "#;
-        assert_eq!(parse_content("12".to_string()), 12)
+        assert_eq!(parse_content(str.to_string()), 22)
     }
 
     #[test]
-    fn parse_content_multi_2() {
+    fn parse_content_examples1() {
         let str = r#"
         1abc2
         pqr3stu8vwx
         a1b2c3d4e5f
         treb7uchet
-        "#.to_string();
+        "#
+        .to_string();
         assert_eq!(parse_content(str), 142)
+    }
+
+    #[test]
+    fn parse_content_examples2() {
+        let str = r#"
+            two1nine
+            eightwothree
+            abcone2threexyz
+            xtwone3four
+            4nineeightseven2
+            zoneight234
+            7pqrstsixteen
+        "#.to_string();
+        assert_eq!(parse_content(str), 281);
     }
 }
